a1d99499d490cfc83a44c857192f0d6911486eff
bigcat88 <bigcat88@icloud.com>
2025-04-19 10:50:29 +0300

    fixed typo in typing
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def temporary_env_var(key: str, new_value: str | None):
             del os.environ[key]
 
 
-def images_to_pillow(images: Tensor | list[Tensor]) -> list[Image]:
+def images_to_pillow(images: Tensor | list[Tensor]) -> list[Image.Image]:
     pillow_images = []
     for _bn, image in enumerate(images):
         i = 255.0 * image.cpu().numpy()
